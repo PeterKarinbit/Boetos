2c9170140b5cd262ffe02c6fb668956e3ccb1cd1
PeterKarinbit <peterkaringithi544@gmail.com>
2025-07-06 07:52:22 +0300

    uiyuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- backend/src/types/express-extensions.d.ts
+++ backend/src/types/express-extensions.d.ts
@@ -4,6 +4,9 @@ declare global {
   namespace Express {
     interface Request {
       user?: CustomUser;
+      isAuthenticated(): boolean;
+      logout(callback?: (err: any) => void): void;
+      session?: any;
     }
   }
 }
